f2fa0b66cd8bdaaefb43453319581c3f21bd0faa
serwreckerrr <trinhbinh9191@gmail.com>
2026-03-23 16:13:56 +0700

    working with new db

--- backend/services/db.js
+++ backend/services/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     port: 3306,             
     user: 'root',
-    password: 'rubi12345678', 
+    password: 'password', 
     database: 'ql_ao_nuoi',
     waitForConnections: true,
     connectionLimit: 10,
